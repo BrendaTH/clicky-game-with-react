c9edea07a90999540be9645fc932d3f6fa8eda8a
generic Mac <genericmac@generics-MacBook-Pro.local>
2019-08-10 11:33:16 -0400

    Updates

---
+++ precache-manifest.b64905d9c20b36ab06863833e5b41f53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df39594190f141669b4b302f10d91970",
+    "url": "/clicky-game-with-react/index.html"
+  },
+  {
+    "revision": "d5c22ad0e60cecbcf01a",
+    "url": "/clicky-game-with-react/static/css/main.caed9542.chunk.css"
+  },
+  {
+    "revision": "ded79a0dfb37e41da216",
+    "url": "/clicky-game-with-react/static/js/2.84ad09b2.chunk.js"
+  },
+  {
+    "revision": "d5c22ad0e60cecbcf01a",
+    "url": "/clicky-game-with-react/static/js/main.fa9d6407.chunk.js"
+  },
+  {
+    "revision": "cd5961edfb406e28d693",
+    "url": "/clicky-game-with-react/static/js/runtime~main.a49ebfe5.js"
+  }
+]);
